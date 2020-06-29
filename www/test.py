594243d2b214729328bcc8aa836d8e8d731e8537
dilless <bohuang1994@gmail.com>
2020-06-29 17:09:09 +0800

    models

---
+++ www/test.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from www import orm
+from www.models import User
+
+
+async def test(loop):
+    await orm.create_pool(loop = loop, user='root', password='root', db='awesome')
+    u = User(name='Test2', email='test2@example.com', passwd='1234567890', image='about:blank')
+    await u.save()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
