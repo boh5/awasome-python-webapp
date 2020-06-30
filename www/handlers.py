849d997305a14b971a4acf630e122e4cdcd7e354
dilless <bohuang1994@gmail.com>
2020-06-30 15:31:14 +0800

    mvc

--- www/handlers.py
+++ www/handlers.py
@@ -1,1 +1,11 @@
-' url handlers '
+from www.coroweb import get
+from www.models import User
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users,
+    }
